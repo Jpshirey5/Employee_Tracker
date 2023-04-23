776229f55d04577af594aa302f12d222a2bf3bb1
John Shirey <you@example.com>
2023-04-22 21:24:30 -0400

    Updated database/seeds

---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO department (name)
+VALUES ("Engineering"), ("Finance"), ("Legal"), ("Sales"), ("Marketing");
+
+INSERT INTO role (title, salary, department_id)
+VALUE ("CEO", 800000.00, 5), 
+("Senior Director", 600000.00, 4), 
+("Sales Representative", 400000.00, 3), 
+("Software Engineer", 300000.00, 1); 
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("John", "Smith", 1, 3), 
+("Julia", "McIntosh", 1, 1), 
+("David", "Spark", 3, 2), 
+("Rockwell", "Kingston", 5, 2), 
