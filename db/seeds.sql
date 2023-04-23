794e62939d89fe5f4d4081993babd3243cf9f5fd
John Shirey <you@example.com>
2023-04-22 22:27:29 -0400

    started server.js

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,11 +4,13 @@ VALUES ("Engineering"), ("Finance"), ("Legal"), ("Sales"), ("Marketing");
 INSERT INTO role (title, salary, department_id)
 VALUE ("CEO", 800000.00, 5), 
 ("Senior Director", 600000.00, 4), 
+("Director", 500000.00, 2), 
 ("Sales Representative", 400000.00, 3), 
 ("Software Engineer", 300000.00, 1); 
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUE ("John", "Smith", 1, 3), 
 ("Julia", "McIntosh", 1, 1), 
-("David", "Spark", 3, NULL), 
+("Sam", "Jones", 1, 2), 
+("David", "Spark", 4, NULL), 
 ("Rockwell", "Kingston", 5, NULL), 
